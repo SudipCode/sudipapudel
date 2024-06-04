707ffdf392ca7fc6a993458c85fa083e0617e356
memory <91591906+SudipCode@users.noreply.github.com>
2024-06-04 14:14:29 +0545

    upp

---
+++ js/tex.js
@@ -0,0 +1,43 @@
+let menu = document.querySelector('#menu-bars');
+let header = document.querySelector('header');
+
+menu.onclick = () =>{
+    menu.classList.toggle('fa-times');
+    header.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+    menu.classList.remove('fa-times');
+    header.classList.remove('active');
+};
+
+var typed = new Typed('.typing-text', {
+    strings : ['front end developer', 'SubEngineer', 'Web designer', 'graphic designer', 'web developer' , 'Programmer'],
+    loop : true,
+    typeSpeed : 30
+});
+
+VanillaTilt.init(document.querySelectorAll('.tilt'),{
+    max:30
+});
+
+window.onload = () =>{
+
+    if(window.scrollY > 80){
+      document.querySelector('.header .header-2').classList.add('active');
+    }else{
+      document.querySelector('.header .header-2').classList.remove('active');
+    }
+  
+    fadeOut();
+  
+  }
+  
+function loader(){
+    document.querySelector('.loader-container').classList.add('active');
+  }
+  
+  function fadeOut(){
+    setTimeout(loader, 4000);
+  }
+  
